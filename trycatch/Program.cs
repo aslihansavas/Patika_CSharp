a8b72be4c73ef72a087e3220b2b5cd7858770636
Aslı Savaş <72549961+aslihansavas@users.noreply.github.com>
2024-03-13 20:15:34 +0300

    Add files via upload

---
+++ trycatch/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace trycatch;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Hata alma ihtimali yüksek olan kod bloğu buraya yazılır.
+            Console.WriteLine("Bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı : " + sayi );
+        }
+        /*catch (Exception ex)
+        {
+            // Herhangi bir hata ile karşılaştığımızda yapmak istediğimiz eylemler buraya yazılır.
+            Console.WriteLine("Hata : " + ex.Message.ToString());
+        
+        }*/
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Veri tipi uygun değil");
+            Console.WriteLine(ex);
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("çok küçük ya da çok büyük bir değer girdiniz ");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            // Her durumda çalışacak kod bloğu - opsiyonel
+            Console.WriteLine("işlem tamamlandı.");
+        }
+
+
+
+    }
+}

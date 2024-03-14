ac6c2c3aec2bc137124b90bce4ccfadddcb783bf
Aslı Savaş <72549961+aslihansavas@users.noreply.github.com>
2024-03-14 19:02:18 +0300

    Add files via upload

---
+++ metotOverloading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace metotOverloading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       //out parametreler
+       string sayi ="999";
+
+       bool sonuc = int.TryParse(sayi,out int outSayi);
+       if(sonuc)
+       {
+        Console.WriteLine("başarılı");
+        Console.WriteLine(outSayi);
+       }
+       else
+       Console.WriteLine("başarısız");
+       Metotlar instance =new Metotlar();
+       instance.Topla(4,5,out int toplamsonucu);
+       Console.WriteLine(toplamsonucu);
+
+       //metot aşırı yükleme - overloading
+       int ifade=999;
+       instance.EkranaYazdir(Convert.ToString(ifade));
+       // Metot imzası 
+       // metotadı + parametre sayisi + parametre
+       instance.EkranaYazdir(ifade);
+       instance.EkranaYazdir("aslihan","savaş");
+       
+    
+
+    }
+    
+}
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam){
+        toplam=a+b;
+    }   
+    
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri1, string veri2){
+        Console.WriteLine(veri1 + veri2);
+    }
+}

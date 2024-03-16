ac2f2ad57b40e5b12c48e1d272e273e6a4b4a4ef
Aslı Savaş <72549961+aslihansavas@users.noreply.github.com>
2024-03-16 03:42:16 +0300

    Add files via upload

---
+++ constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace constructor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calisan calisan1=new Calisan("aslı","savaş");
+    }
+}
+class Calisan{
+    public string Ad;
+    public string Soyad;
+
+    public Calisan(string ad, string soyad)
+    {
+        this.Ad=ad;
+        this.Soyad=soyad;
+
+    }
+}
